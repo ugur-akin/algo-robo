b51dd9043fe4477d980dc4d664c0642523a8b622
Ugur Akin <ugurakinsoftware@gmail.com>
2020-11-28 15:36:56 +0300

    Centering Box now accepts a prop to determine whether it should expand or not

--- src/components/CenteringBox.jsx
+++ src/components/CenteringBox.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import {Box} from '@material-ui/core';
 
-const CenteringBox = React.forwardRef(({children, ...props}, ref) => {
-  return (
-    <Box
-      height="100%"
-      width="100%"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      ref={ref}
-      {...props}
-    >
-      {children}
-    </Box>
-  );
-});
+const CenteringBox = React.forwardRef(
+  ({children, expand = false, ...props}, ref) => {
+    const expandProps = expand ? {height: '100%', width: '100%'} : {};
+
+    return (
+      <Box
+        {...expandProps}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </Box>
+    );
+  },
+);
 
 export default CenteringBox;
